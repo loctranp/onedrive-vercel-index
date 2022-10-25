c4aeab7d7e1d1f1e194ad9405abf3acb586bd370
loctranp <107838399+loctranp@users.noreply.github.com>
2022-10-25 22:00:25 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -41,7 +41,7 @@ module.exports = {
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
   protectedRoutes: [
-    '/Personal/Portfolio', 
+    '/', 
   ],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
